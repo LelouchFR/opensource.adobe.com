3732ee2e0659f5c31ef6dc4080ab48efbc2e05ab
LelouchFR <baptistezahnow@gmail.com>
2024-08-12 18:41:46 +0200

    header start & added helmet async for seo

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
     return (
-        <p>Hoempage</p>
+        <p>Homepage</p>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,66 @@
-export default function Header() {
+import { ReactElement, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import AdobeLogo from "../assets/svg/adobe.svg";
+import BurgerMenu from "../assets/svg/burger-menu.svg";
+import CrossMenu from "../assets/svg/cross.svg";
+import GridMenu from "../assets/svg/menu-grid.svg";
+
+export default function Header(): ReactElement {
+    const [width, setWidth] = useState<number>(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [innerWidth]);
+
+    return width > 900 ? <DesktopHeader /> : <PhoneHeader />
+}
+
+function PhoneHeader(): ReactElement {
+    const [isMenuOpen, setIsMenuOpen] = useState<bool>(false);
+
     return (
-        <p>Header</p>
+        <header>
+            <div>
+                <img 
+                    onClick={() => setIsMenuOpen(!isMenuOpen)}
+                    src={isMenuOpen ? CrossMenu : BurgerMenu}
+                />
+                <Link to="/">
+                    <img src={AdobeLogo} />
+                    <p>Adobe</p>
+                </Link>
+            </div>
+            <div>
+                <img src={GridMenu} />
+                <button>Sign in</button>
+            </div>
+        </header>
+    )
+}
+
+function DesktopHeader(): ReactElement {
+    return (
+        <header>
+            <div>
+                <Link to="/">
+                    <img src={AdobeLogo} />
+                    <p>Adobe Open Source</p>
+                </Link>
+                <nav>
+                    {["Creativity & Design", "Marketing & Commerce", "Help & Support"].map((summary: string, index: number): ReactElement => 
+                        <details key={index}>
+                            <summary>{summary}</summary>
+                            <section></section>
+                        </details>
+                    )}
+                </nav>
+            </div>
+            <div>
+                <img src={GridMenu} />
+                <button>Sign in</button>
+            </div>
+        </header>
     )
 }
